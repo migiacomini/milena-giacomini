f6b429eca7e2b4dbeb911bbdea52658151a1f2ba
Milena Giacomini <acadmilena@gmail.com>
2026-07-07 16:54:39 -0300

    última att

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://migiacomini.github.io',
-  base: '/milena-giacomini/',
+  base: '/milena-giacomini',
 });
